d1f845275fc9df815cd5d929d5c01e5a26f0907a
Saxsori <jungxbra@gmail.com>
2022-10-05 20:25:48 +0000

    fixing replace var

--- CPP_MODULE_01/ex044/main.cpp
+++ CPP_MODULE_01/ex044/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaljaber <aaljaber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aaljaber <aaljaber@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 17:14:22 by aaljaber          #+#    #+#             */
-/*   Updated: 2022/10/05 20:08:23 by aaljaber         ###   ########.fr       */
+/*   Updated: 2022/10/05 20:24:50 by aaljaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,59 +17,74 @@
 std::string replaceString(std::string line, std::string s1, std::string s2)
 {
 	std::string newline = line;
-	std::cout << "-->" << newline << std::endl;
+	int index;
 	int	found = line.find(s1);
-	std::cout << "pos " << found << std::endl;
-	newline.erase(found, s1.size());
-	newline.insert(found, s2);
-	found = line.find(s1);
 	int first = found;
 	int i = 0;
+	int	diff = 0;
+
+	if (s1.length() > s2.length())
+		diff = (s1.length() - s2.length());
+	else if (s1.length() < s2.length() )
+		diff = (s2.length() - s1.length());
 	while (1)
 	{
-		std::cout << "pos " << found << std::endl;
-		std::cout << found - (i++) << std::endl;
+		if (s1.length() < s2.length())
+			index = found + i;
+		else if (s1.length() > s2.length())
+			index = found - i;
+		else
+			index = found;
+		std::cout << "pos " << index << std::endl;
+		newline.erase(index, s1.length());
+		newline.insert(index, s2);
 		found = line.find(s1, found + 1);
 		if (found == std::string::npos || found == first)
 			break ;
-		// found++;
-		// newline.erase(found, s1.size());
-		// newline.insert(found, s2);
-		// std::cout << "-->" << newline << std::endl;
-		// found += (found - s2.size());
+		i = i + diff;
 	}
+	std::cout << "-->" << newline << std::endl;
 	return (newline);
 }
 
+					// 	// std::cout << "what is in the file --> " << BRED << line << DEFCOLO << std::endl;
+					// 	// new_line = replaceString("andandh", "and", "andy");
+					// 	// new_line = replaceString("booboon", "boo", "boom");
+					// 	// new_line = replaceString("booboon", "boo", "boom");
+					// 	// new_line = replaceString("booboolalaboobooboolllbooboommboonboobooboonnboo", "boo", "ABUUU");
 int main (int ac, char **ag)
 {
-	// ErrMng		errMng;
-	// std::string line;
+	ErrMng		errMng;
+	std::string line;
 	std::string	new_line;
 
-	// if (errMng.ArgNumCheck(ac))
-	// {
-	// 	if (errMng.isArg(ag[1], ag[2], ag[3]))
-	// 	{
-	// 		std::ifstream read(ag[1]);
-	// 		if (read.is_open())
-	// 		{
-	// 			std::ofstream write(errMng.fileName(ag[1]), std::ios::app);
-	// 			while (read.good())
-	// 			{
-	// 				std::getline(read, line);
-	// 				if (line.empty())
-	// 					break ;
-	// 				std::cout << "what is in the file --> " << BRED << line << DEFCOLO << std::endl;
-					// new_line = replaceString("andandh", "and", "andy");
-					// new_line = replaceString("booboon", "boo", "boom");
-					new_line = replaceString("booboolalaboobooboolllbooboommboonboobooboonnboo", "boo", "bo");
-					// std::cout << "new line --> " << BRED << new_line << DEFCOLO << std::endl;
-	// 			}
-	// 			read.close();
-	// 		}
-	// 		else
-	// 			std::cout << BRED << "file doesn't exist" << DEFCOLO << std::endl;
-	// 	}	
-	// }
+	if (errMng.ArgNumCheck(ac))
+	{
+		if (errMng.isArg(ag[1], ag[2], ag[3]))
+		{
+			std::ifstream read(ag[1]);
+			if (read.is_open())
+			{
+				if ((read.rdstate() & std::ifstream::failbit ) == 0)
+				{
+					std::cout << "fail " << read.failbit << " bad " << read.badbit << " good " << read.goodbit << std::endl; 
+					std::ofstream write(errMng.fileName(ag[1]), std::ios::app);
+					while (read.good())
+					{
+						std::getline(read, line);
+						if (line.empty())
+							break ;
+						new_line = replaceString(line, ag[2], ag[3]);
+						write << new_line;
+						std::cout << "new line --> " << BRED << new_line << DEFCOLO << std::endl;
+					}
+					read.close();
+				}
+				else
+					std::cout << BRED << "file isn't readable or empty" << DEFCOLO << std::endl;
+			}
+			else
+				std::cout << BRED << "file doesn't exist or not readable" << DEFCOLO << std::endl;
+		}	
+	}
 }
