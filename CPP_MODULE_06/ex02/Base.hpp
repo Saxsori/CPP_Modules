8bf2a18a32f43b4401d4f5630d0641349521ec91
Abrar Aljaberi <aaljaber@lab3r8s10.42abudhabi.ae>
2022-10-18 07:46:00 +0400

    final 06

--- CPP_MODULE_06/ex02/Base.hpp
+++ CPP_MODULE_06/ex02/Base.hpp
@@ -6,12 +6,14 @@
 /*   By: aaljaber <aaljaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 07:32:02 by aaljaber          #+#    #+#             */
-/*   Updated: 2022/10/18 07:36:44 by aaljaber         ###   ########.fr       */
+/*   Updated: 2022/10/18 07:45:19 by aaljaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 #define BASE_HPP
+#include <iostream>
+#include <stdlib.h> 
 
 class Base
 {
